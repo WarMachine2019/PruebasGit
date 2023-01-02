0717dad186722c7088c05d69e9bd48f1a9dd1d77
Caleb Rodriguez <josue_caleb1995@hotmail.com>
2023-01-02 17:49:42 -0600

    commit prueba 3

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>HOLA MUNDO 2.0</h1>
+        <h2>HOLA MUNDO 3.0</h2>
       </header>
     </div>
   );
